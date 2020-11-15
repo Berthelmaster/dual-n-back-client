b13f38599d77ac2dfb8a7104e35cea382d134e42
Berthelmaster <berthel0802@gmail.com>
2020-11-15 20:05:37 +0100

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+const useStyles = makeStyles({
+    root: {
+        position: "fixed",
+        bottom: 0,
+        textAlign: "center",
+        paddingBottom: 10,
+        width: '100%'
+    },
+  });
+
+
 
 export default function Footer() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
     return (
-        <div>
-            This is the footer
-        </div>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue)
+      }}
+      showLabels
+      className={classes.root}
+    >
+        <h4>The footer</h4>
+    </BottomNavigation>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import Button from '@material-ui/core/Button';  
 import Menu from '@material-ui/core/Menu';  
 import MenuItem from '@material-ui/core/MenuItem';  
 import AppBar from '@material-ui/core/AppBar';  
 import Toolbar from '@material-ui/core/Toolbar';  
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
-import { Divider, IconButton } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
