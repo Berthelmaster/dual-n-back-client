6b617a6ea79a80530035dbc87ab39ef164585a5e
Berthelmaster <berthel0802@gmail.com>
2020-12-05 17:00:27 +0100

    ws now works

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -56,7 +56,9 @@ const useStyles = theme => ({
             isRunning: false,
             gameList: [],
             playerScore: 0,
-            highScore: 0
+            highScore: 0,
+            creditedPlayer: 'unknown',
+            ws: null
         }
 
         this.stop = this.stop.bind(this)
@@ -64,6 +66,70 @@ const useStyles = theme => ({
         this.positionPressed = this.positionPressed.bind(this)
         this.soundPressed = this.soundPressed.bind(this)
         this.bothPressed = this.bothPressed.bind(this)
+    };
+
+    componentDidMount() {
+        this.connect();
+    }
+
+    timeout = 250; // Initial timeout duration as a class variable
+
+    /**
+     * @function connect
+     * This function establishes the connect with the websocket and also ensures constant reconnection if connection closes
+     */
+    connect = () => {
+        var ws = new WebSocket("ws://127.0.0.1:3000/");
+        let that = this; // cache the this
+        var connectInterval;
+
+        // websocket onopen event listener
+        ws.onopen = () => {
+            console.log("connected websocket main component");
+
+            this.setState({ ws: ws });
+
+            that.timeout = 250; // reset timer to 250 on open of websocket connection 
+            clearTimeout(connectInterval); // clear Interval on on open of websocket connection
+        };
+
+        ws.onmessage = evt => {
+            // listen to data sent from the websocket server
+            console.log(evt.data)
+        }
+
+        // websocket onclose event listener
+        ws.onclose = e => {
+            console.log(
+                `Socket is closed. Reconnect will be attempted in ${Math.min(
+                    10000 / 1000,
+                    (that.timeout + that.timeout) / 1000
+                )} second.`,
+                e.reason
+            );
+
+            that.timeout = that.timeout + that.timeout; //increment retry interval
+            connectInterval = setTimeout(this.check, Math.min(10000, that.timeout)); //call check function after timeout
+        };
+
+        // websocket onerror event listener
+        ws.onerror = err => {
+            console.error(
+                "Socket encountered error: ",
+                err.message,
+                "Closing socket"
+            );
+
+            ws.close();
+        };
+    };
+
+    /**
+     * utilited by the @function connect to check if the connection is close, if so attempts to reconnect
+     */
+    check = () => {
+        const { ws } = this.state;
+        if (!ws || ws.readyState == WebSocket.CLOSED) this.connect(); //check if websocket instance is closed, if so call `connect` function.
     };
 
 
@@ -314,7 +380,7 @@ const useStyles = theme => ({
             justify="center"
             alignItems="center">
             <h4 className={classes.buttonScore}>You Score <b id="playerscore">{this.state.playerScore}</b> points</h4>
-            <h4 className={classes.buttonScore}>Current highscore <b id="highscore">{this.state.highScore}</b> points</h4>
+            <h4 className={classes.buttonScore}>Current highscore <b id="highscore">{this.state.highScore}</b> points by {this.state.creditedPlayer}</h4>
             </Grid>
             <div>
                 <Button variant="contained" color="primary" onClick={this.play}>Play</Button>
